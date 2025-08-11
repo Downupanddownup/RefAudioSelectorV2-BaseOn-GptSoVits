0e961be1732e52aef23b92d1b245b8db7d9a38e7
io2o <pingdengjia0liu@163.com>
2025-08-11 21:43:29 +0800

    --

--- web/config.js
+++ web/config.js
@@ -91,7 +91,7 @@ const SysConfig = (function () {
         'inpRefsList': '融合音频，在【参考音频】tab下，点击【融合音频】进入管理界面,可选项：通过选择多个音频（建议同性），平均融合他们的音色。如不选择此项，音色由参考音频控制。如是微调模型，建议参考音频全部在微调训练集音色内，底模不用管。',
      }
     
-    const defaultGptSovitsVersion = 'v3'
+    const defaultGptSovitsVersion = 'v2ProPlus'
     // const defaultTopK = 12
     const defaultTopK = 15
     // const defaultTopP = 0.6
